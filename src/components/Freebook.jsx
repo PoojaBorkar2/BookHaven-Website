6010a2042c9bc3cf3bc9fe8b67aeb304aa79a38d
PoojaBorkar2 <poojab2805@gmail.com>
2025-06-27 15:02:49 +0530

    the fourth commit is done

---
+++ src/components/Freebook.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import list from "../../public/list.json";
+function Freebook() {
+  const filterData = list.filter((data) => data.category === "Free");
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="fotn-semibold text-xl pb-2">Free Offered Courses</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+            provident ad, sit ratione doloremque repellendus eaque modi tempore
+            velit nesciunt iusto rerum consectetur qui recusandae.
+          </p>
+        </div>
+
+        <div>
+          <Slider className="" {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+            <div>
+              <h3>7</h3>
+            </div>
+            <div>
+              <h3>8</h3>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Freebook;
