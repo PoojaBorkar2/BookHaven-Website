4dda2e1840a80d8fa61be25a55965f6178af374e
PoojaBorkar2 <poojab2805@gmail.com>
2025-06-27 15:23:52 +0530

    the sixth commit is done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import Freebook from './components/Freebook'
+import Cards from './components/Cards'
+
+const App = () => {
+  return (
+<>
+<Navbar/>
+<Banner/>
+<Freebook/>
+<Cards/>
+<Footer/>
+</>
+  )
+}
+
+export default App
